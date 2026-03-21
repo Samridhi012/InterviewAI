70c2fec96d882157a1a75294d6f1d7862a5b8d51
Samridhi <sharmasamridhi68@gmail.com>
2026-03-21 23:45:09 +0530

    AI related backend work started

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors= require('cors');
 
 //initiate server
 const app = express();
@@ -7,7 +8,10 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(cookieParser()); //middleware to parse cookies from incoming requests
-
+app.use(cors({
+    origin: 'http://localhost:5173', //frontend url
+    credentials: true, //allow cookies to be sent in cross-origin requests
+}))
 //routes
 const authRouter = require('./routes/auth.routes');
 
