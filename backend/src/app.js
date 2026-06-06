1e2d342a309f82fb8af79f79a73528b57b27e710
Samridhi <sharmasamridhi68@gmail.com>
2026-06-06 22:08:14 +0530

    interview report's code done on backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,5 +20,5 @@ const interviewRouter = require('./routes/interview.routes');
 
 //using all the routes here
 app.use('/api/auth', authRouter);
-
+app.use('/api/interview', interviewRouter);
 module.exports = app;
